8f15d8a270f6e4c770d38c3a78e4af151598c1c3
DiegoAndre Leffa <diegodocanto@gmail.com>
2023-10-06 09:12:53 -0300

    feat: create tables pokemons/types/pokemons_type

---
+++ database/migrations/2023_10_06_115245_create_pokemons_types_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePokemonsTypesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pokemons_types', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_pokemon');
+            $table->integer('id_type');
+
+            $table->foreign('id_pokemon')->references('id')->on('pokemons');
+            $table->foreign('id_type')->references('id')->on('types');
+
+            $table->unique(['id_pokemon', 'id_type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pokemons_types');
+    }
+}
